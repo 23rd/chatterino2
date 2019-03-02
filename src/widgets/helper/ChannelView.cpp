bb15cd2eecae7b9ca976ae0d0d39b9e91460974f
23rd <23rd@vivaldi.net>
2019-03-02 12:27:48 +0300

    Removed "@" from mention when use right click.

--- src/widgets/helper/ChannelView.cpp
+++ src/widgets/helper/ChannelView.cpp
@@ -2441,7 +2441,7 @@ void ChannelView::handleMouseClick(QMouseEvent *event,
                                 split->getInput().isEditFirstWord();
                             auto userMention = formatUserMention(
                                 link.value, isFirstWord, commaMention);
-                            insertText("@" + userMention + " ");
+                            insertText(userMention + " ");
                         }
                         break;
 
