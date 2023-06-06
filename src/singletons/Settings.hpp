dfe9efeae425897caa2f298ecc44fe3c7000da81
23rd <23rd@vivaldi.net>
2023-06-06 03:25:00 +0300

    Added ability to pass channel name to custom bash script.

--- src/singletons/Settings.hpp
+++ src/singletons/Settings.hpp
@@ -585,6 +585,7 @@ public:
 
     // Custom URI Scheme
     QStringSetting customURIScheme = {"/external/urischeme"};
+    QStringSetting customBashRunFile = {"/external/customBashRunFile"};
 
     // Image Uploader
     BoolSetting imageUploaderEnabled = {"/external/imageUploader/enabled",
